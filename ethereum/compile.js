bd614e4256593eee571e9a6921cf1321ce1358d4
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-02 17:37:23 -0500

    fixed naming error

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -5,13 +5,13 @@ const solc = require('solc');
 const buildPath = path.resolve(__dirname, 'build');
 fs.removeSync(buildPath);
 
-const timeVaultPath = path.resolve(__dirname, 'contracts', 'TimeVault.sol');
-const source = fs.readFileSync(timeVaultPath, 'utf8');
+const timedVaultPath = path.resolve(__dirname, 'contracts', 'TimedVault.sol');
+const source = fs.readFileSync(timedVaultPath, 'utf8');
 
 const input = {
   language: 'Solidity',
   sources: {
-    'TimeVault.sol': {
+    'TimedVault.sol': {
       content: source,
     },
   },
@@ -28,6 +28,6 @@ const output = JSON.parse(solc.compile(JSON.stringify(input)));
 fs.ensureDirSync(buildPath);
 
 fs.outputJsonSync(
-    path.resolve(buildPath, 'TimeVault.json'),
-    output.contracts['TimeVault.sol']['TimeVault']
+    path.resolve(buildPath, 'TimedVault.json'),
+    output.contracts['TimedVault.sol']['TimedVault']
 );
