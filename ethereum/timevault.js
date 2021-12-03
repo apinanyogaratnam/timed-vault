0bfb739d3e4dd50c644d14c3a093c3c229a5500d
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-02 20:39:25 -0500

    created metamask connect button

--- ethereum/timevault.js
+++ ethereum/timevault.js
@@ -1,7 +1,8 @@
-import web3 from './web3';
+import connectToMetaMask from './web3';
 import TimedVault from '../ethereum/build/TimedVault.json';
 
 const connect = () => {
+    const web3 = connectToMetaMask();
     return new web3.eth.Contract(TimedVault.abi, '0xAD69E3De929f34B3dE1BB89C00663331D376f61E');
 };
 
