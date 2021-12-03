bca590d49bbca62f71c89bf63117f65c6ee98699
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-02 20:54:23 -0500

    displaying address after connected with a manual reload

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,25 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import connect from '../ethereum/timeVault';
 import { useState } from 'react';
+import connectWeb3 from '../ethereum/web3';
 
 export default function Home() {
     const [isConnected, setIsConnected] = useState(false);
     const [instance, setInstance] = useState(null);
+    const [address, setAddress] = useState(null);
 
-    const connectToMetaMask = () => {
+    const connectToMetaMask = async () => {
         const contractInstance = connect();
         setInstance(contractInstance);
         setIsConnected(true);
+        const accounts = await connectWeb3().eth.getAccounts();
+        setAddress(accounts[0]);
     };
 
     return (
         <div>
-            <button onClick={connectToMetaMask}>Connect to MetaMask</button>
+            { isConnected ? <button>{address}</button> : <button onClick={connectToMetaMask}>Connect to MetaMask</button>}
             {isConnected ? <h1>connected</h1> : <h1>not connected</h1>}
-            <h1>asdf</h1>
         </div>
     );
 }; 
