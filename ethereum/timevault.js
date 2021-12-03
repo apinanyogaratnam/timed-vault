a5506d05299030d8612685068807bc95f780224a
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-02 20:07:30 -0500

    changed script to function to call and not request metamask immediately

--- ethereum/timevault.js
+++ ethereum/timevault.js
@@ -1,6 +1,8 @@
 import web3 from './web3';
 import TimedVault from '../ethereum/build/TimedVault.json';
 
-const instance = new web3.eth.Contract(TimedVault.abi, '0xAD69E3De929f34B3dE1BB89C00663331D376f61E');
+const connect = () => {
+    return new web3.eth.Contract(TimedVault.abi, '0xAD69E3De929f34B3dE1BB89C00663331D376f61E');
+};
 
-return instance;
+export default connect;
