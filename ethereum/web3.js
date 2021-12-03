db4ffbde617066bac369a0e768201dda1412d425
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-02 20:05:01 -0500

    removed commented code for web3 file

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -2,22 +2,6 @@ import Web3 from 'web3';
 
 let web3;
 
-// const connect = () => {
-//     if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
-//         window.ethereum.request({ method: 'eth_requestAccounts' });
-//         web3 = new Web3(window.ethereum);
-//     } else {
-//         const provider = new Web3.providers.HttpProvider(
-//             "https://rinkeby.infura.io/v3/d55c4f57b80445d284aa7754f355aaeb"
-//         );
-//         web3 = new Web3(provider);
-//     }
-
-//     return web3;
-// }
-
-// export default connect;
-
 if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     window.ethereum.request({ method: 'eth_requestAccounts' });
     web3 = new Web3(window.ethereum);
