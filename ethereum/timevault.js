e89a34aa1f392ca676cb5380c53ed17fe301582f
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-02 20:04:10 -0500

    created time vault instance script

---
+++ ethereum/timevault.js
@@ -0,0 +1,6 @@
+import web3 from './web3';
+import TimedVault from '../ethereum/build/TimedVault.json';
+
+const instance = new web3.eth.Contract(TimedVault.abi, '0xAD69E3De929f34B3dE1BB89C00663331D376f61E');
+
+return instance;
